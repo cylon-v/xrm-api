13da9664fbb08752fd3b4a1e96fc8e154af70a8a
Vova Kalinkin <vova.kalinkin@gmail.com>
2019-03-08 19:35:16 +0300

    Refactoring.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import Util from './util';
-import Message from './message';
+import Serializer from 'serializer';
+
+
+const serializer = new Serializer();
 
 export class Dynamics {
   constructor(settings) {
@@ -8,38 +10,103 @@ export class Dynamics {
   }
 
   create(options) {
-    return this.message.Create(options);
+    const template = `
+			<s:Body>
+				<Create xmlns="http://schemas.microsoft.com/xrm/2011/Contracts/Services"  xmlns:b="http://schemas.microsoft.com/xrm/2011/Contracts" xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns:c="http://schemas.datacontract.org/2004/07/System.Collections.Generic">
+					<entity>
+						{requetbody}
+					</entity>
+				</Create>
+			</s:Body>`;
+
+    return util.executePostPromised(options, "Create", template, serializer.toXmlCreateUpdate(options));
   }
 
   update(options) {
-    return this.message.Update(options);
+    const template = `
+			<s:Body>
+				<Update xmlns="http://schemas.microsoft.com/xrm/2011/Contracts/Services"  xmlns:b="http://schemas.microsoft.com/xrm/2011/Contracts" xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns:c="http://schemas.datacontract.org/2004/07/System.Collections.Generic">
+					<entity>
+						{requetbody}
+					</entity>
+				</Update>
+			</s:Body>`;
+
+    return util.executePostPromised(options, "Update", template, serializer.toXmlCreateUpdate(options));
   }
 
   retrieve(options) {
-    return this.message.Retrieve(options);
+    const template = `
+			<s:Body>
+				<Retrieve xmlns="http://schemas.microsoft.com/xrm/2011/Contracts/Services"  xmlns:b="http://schemas.microsoft.com/xrm/2011/Contracts" xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns:c="http://schemas.microsoft.com/2003/10/Serialization/Arrays">
+					{requetbody}
+				</Retrieve>
+			</s:Body>`;
+
+    return util.executePostPromised(options, "Retrieve", template, serializer.toXmlRetrieve(options));
   }
 
   retrieveMultiple(options) {
-    return this.message.RetrieveMultiple(options);
+    const template = `
+			<s:Body>
+				<RetrieveMultiple xmlns="http://schemas.microsoft.com/xrm/2011/Contracts/Services">
+					<query 
+						i:type="b:QueryExpression"
+						xmlns:b="http://schemas.microsoft.com/xrm/2011/Contracts"
+						xmlns:i="http://www.w3.org/2001/XMLSchema-instance"
+						xmlns:c="http://schemas.microsoft.com/2003/10/Serialization/Arrays">
+						{requetbody}
+					</query>
+				</RetrieveMultiple>
+			</s:Body>`;
+
+    return executePostPromised(options, "RetrieveMultiple", template, serializer.toXmlRetrieveMultiple(options));
   }
 
   associate(options) {
-    return this.message.Associate(options);
+    const template = `
+			<s:Body>
+				<Associate xmlns="http://schemas.microsoft.com/xrm/2011/Contracts/Services"  xmlns:b="http://schemas.microsoft.com/xrm/2011/Contracts" xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns:c="http://schemas.datacontract.org/2004/07/System.Collections.Generic">
+					{requetbody}
+				</Associate>
+			</s:Body>`;
+
+    return util.executePostPromised(options, "Associate", template, serializer.toXmlAssociate(options));
   }
 
   disassociate(options) {
-    return this.message.Disassociate(options);
+    const template = `
+			<s:Body>
+				<Disassociate xmlns="http://schemas.microsoft.com/xrm/2011/Contracts/Services"  xmlns:b="http://schemas.microsoft.com/xrm/2011/Contracts" xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns:c="http://schemas.datacontract.org/2004/07/System.Collections.Generic">
+					{requetbody}
+				</Disassociate>
+			</s:Body>`;
+
+    return util.executePostPromised(options, "Disassociate", template, serializer.toXmlAssociate(options));
   }
 
   execute(options) {
-    return this.message.Execute(options);
-  }
+    var template = `
+			<s:Body>
+				<Execute xmlns="http://schemas.microsoft.com/xrm/2011/Contracts/Services"  xmlns:b="http://schemas.microsoft.com/xrm/2011/Contracts" xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns:c="http://schemas.datacontract.org/2004/07/System.Collections.Generic">
+					<!--Optional:-->
+					<request>
+						{requetbody}
+					</request>
+				</Execute>
+			</s:Body>`;
 
-  executeSetState(options) {
-    return this.message.ExecuteSetState(options);
+    return util.executePostPromised(options, "Execute", template, serializer.toXmlExecute(options));
   }
 
   delete(options) {
-    return this.message.Delete(options);
+    const template = `
+			<s:Body>
+				<Delete xmlns="http://schemas.microsoft.com/xrm/2011/Contracts/Services"  xmlns:b="http://schemas.microsoft.com/xrm/2011/Contracts" xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns:c="http://schemas.datacontract.org/2004/07/System.Collections.Generic">
+					{requetbody}
+				</Delete>
+			</s:Body>`;
+
+    return util.executePostPromised(options, "Delete", template, serializer.toXmlDelete(options));
   }
 }
